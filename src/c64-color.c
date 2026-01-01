ebf5c50fd8668a1827e097998621369b2e418b8a
Christian Gleissner <chrisgleissner@users.noreply.github.com>
2026-01-01 01:29:40 +0000

    Refine VIC-II color palette entries
    Updated color values in the VIC-II color palette for OBS Studio.

--- src/c64-color.c
+++ src/c64-color.c
@@ -14,18 +14,18 @@ See <https://www.gnu.org/licenses/> for details.
 // Colors converted from C64 Ultimate grab.py RGB values to BGRA with full alpha
 const uint32_t vic_colors[16] = {
     0xFF000000, // 0: Black
-    0xFFEFEFEF, // 1: White
+    0xFFF7F7F7, // 1: White
     0xFF342F8D, // 2: Red
     0xFFCDD46A, // 3: Cyan
-    0xFFA43598, // 4: Purple/Magenta
+    0xFFA43598, // 4: Purple
     0xFF42B44C, // 5: Green
     0xFFB1292C, // 6: Blue
     0xFF5DEFEF, // 7: Yellow
     0xFF204E98, // 8: Orange
     0xFF00385B, // 9: Brown
-    0xFF6D67D1, // 10: Light Red
+    0xFF6D67D1, // 10: Pink
     0xFF4A4A4A, // 11: Dark Grey
-    0xFF7B7B7B, // 12: Mid Grey
+    0xFF7B7B7B, // 12: Medium Grey
     0xFF93EF9F, // 13: Light Green
     0xFFEF6A6D, // 14: Light Blue
     0xFFB2B2B2  // 15: Light Grey
